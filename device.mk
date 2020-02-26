f8e7e0c88bb1370d7e7cb1bd99e4250924b57ee6
Alessandro Astone <ales.astone@gmail.com>
2020-02-26 15:09:19 +0100

    TP1803: Build and enable fastbootd
    Change-Id: I0b20600fe7203a7aec19cbea8f6849052585c6ab

--- device.mk
+++ device.mk
@@ -132,6 +132,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm-service.clearkey
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service
