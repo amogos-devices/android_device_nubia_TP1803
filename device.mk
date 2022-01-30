3014356671a181010aebec1e4f4753786fd6cbb4
Timi Rautamäki <timi.rautamaki@gmail.com>
2022-01-30 08:27:29 +0000

    TP1803: Drop Snap
    It's not included anymore since LineageOS 19
    
    Change-Id: Ie3f908234cf7a3d94d33ad0e9d170d3738bb9752

--- device.mk
+++ device.mk
@@ -98,9 +98,6 @@ PRODUCT_PACKAGES += \
     libcomparetf2 \
     libprotobuf-cpp-full-vendorcompat
 
-PRODUCT_PACKAGES += \
-    Snap
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
